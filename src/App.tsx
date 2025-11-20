cbce6db57084a5ca0d300dc13f028f59911885b1
Pradeep479182 <pradeeprakavi@gmail.com>
2025-11-20 19:12:20 +0530

    feat(Ui) : i have done login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import Login from "./pages/login/login"
+
 const App = () => {
   return (
-    <div className="text-3xl font-bold text-teal-800 underline">
-      Project is working!
-    </div>
+    <Login/>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
