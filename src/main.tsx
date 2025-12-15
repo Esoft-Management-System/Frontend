52316fd3ac43737df77a5da45a6830ac0635a014
Harishanth03 <kandeepanharishanth@gmail.com>
2025-12-15 13:34:40 +0530

    feat(redux): Implement RTKProvider and add custom hooks for dispatch and selector

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { RTKProvider } from './redux/provider.ts'
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
   //   <App />
   // </StrictMode>,
   <BrowserRouter>
-    <App />
+    <RTKProvider>
+       <App />
+    </RTKProvider>
   </BrowserRouter>
 )
