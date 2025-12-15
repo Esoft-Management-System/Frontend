aef4fc6627a5bf9e183a77788145416da95122f1
Harishanth03 <kandeepanharishanth@gmail.com>
2025-12-15 08:13:12 +0530

    feat(api): Implement API request service with axios and interceptors

---
+++ src/configs/api-configs.ts
@@ -0,0 +1,5 @@
+export const API_BASE_URL: string = import.meta.env.BASE_URL // i take the base url for all the configuration
+export const API_PREFIX: string = '/api'
+
+//full api URL
+export const API_URL = `${API_BASE_URL}${API_PREFIX}`
