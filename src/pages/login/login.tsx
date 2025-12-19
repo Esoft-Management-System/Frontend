a35ad1d42cffdd87fe9a462faf3048c10470e5a3
Harishanth03 <kandeepanharishanth@gmail.com>
2025-12-19 22:43:52 +0530

    fix(Login, StaffLogin): Update background color to white for improved visibility

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -87,7 +87,7 @@ const Login = () => {
   }
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-linear-to-brrom-gray-50 to-gray-100 overflow-x-hidden">
+    <div className="min-h-screen w-full flex items-center justify-center bg-[#FFFFFF] overflow-x-hidden">
       <div className="flex w-full max-w-md flex-col p-6">
         <form onSubmit={handleSignIn} className="bg-white flex-1 flex flex-col gap-6 p-8 rounded-2xl shadow-lg border border-gray-100">
           {/* Header Section */}
